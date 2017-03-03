023c42685c176c041dd504c2a0626bbf1578c5b7
Vighnesh <vighneshvelayudhan@gmail.com>
2017-03-04 02:40:49 +0530

    solution uploaded

---
+++ solution.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<math.h>
+
+/*
+ * mul returns the product of (count) consecutive numbers before (number)
+ * eg mul(a,b) gives a*(a-1)*...*(a-b)
+ */
+long long mul(long long number,long long count){
+    if(count == 1)
+        return number;
+    return number*mul(number-1,count-1);
+}
+/*
+ * sum_comb returns the sequence (num)C(count)+(num-1)C(count)+...+(num-count+1)C(count)
+ */
+long long sum_comb(long long num, long long count){
+    long long sum = 0;
+    long long multiplier = mul(num,count);
+    long long i=0;
+    for(i = 0; i< count; i++){
+        sum+=multiplier;
+        multiplier = multiplier*(num-count-i)/(num-i);
+    }
+    return sum/mul(count,count);
+}
+
+void main(){
+    /*
+     * t = number of test cases
+     * n = number
+     */
+    int t = 1;
+    long long n,i;
+    long long answer;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d",&n);
+        if(n%2 == 0){
+            answer = pow(2,n-1) + mul(n,n/2)/(2*mul(n/2,n/2));
+        }
+        else
+        {
+            answer = pow(2,n-1)+mul(n,n/2)/mul(n/2,n/2);
+        }
+        for(i=1;i<=(n-1)/2;i++){
+            answer-=sum_comb(n-1,i);
+        }
+        printf("%d\n",answer%(long long)pow(10,9));
+    }
+}
